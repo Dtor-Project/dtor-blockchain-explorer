37b8a75329cde01c21e0e0e895c5495b059b39eb
Dtor-Project <67499509+Dtor-Project@users.noreply.github.com>
2020-08-23 20:11:38 -0700

    Update MempoolStatus.cpp

--- src/MempoolStatus.cpp
+++ src/MempoolStatus.cpp
@@ -325,7 +325,7 @@ MempoolStatus::is_thread_running()
     return is_running;
 }
 
-bf::path MempoolStatus::blockchain_path {"/home/mwo/.dtor/lmdb"};
+bf::path MempoolStatus::blockchain_path {"/home/.dtor/lmdb"};
 string MempoolStatus::deamon_url {"http:://127.0.0.1:23467"};
 cryptonote::network_type MempoolStatus::nettype {cryptonote::network_type::MAINNET};
 atomic<bool>       MempoolStatus::is_running {false};
